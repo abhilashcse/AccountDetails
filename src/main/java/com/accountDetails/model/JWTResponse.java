feaf9c3162cd7af770aa0e95593fcacc79e2b587
shaym kumar chukka <shaymkumarchukka143@gmail.com>
2022-10-07 14:18:02 +0530

    commit by abhilash

--- src/main/java/com/accountDetails/model/JWTResponse.java
+++ src/main/java/com/accountDetails/model/JWTResponse.java
@@ -15,4 +15,22 @@ import lombok.Setter;
 public class JWTResponse {
 	private String token;
 
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public JWTResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public JWTResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
